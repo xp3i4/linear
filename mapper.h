3386365adbe5dfdb9e270a940316f1d0c36eb6ce
xp3i4 <cxpan1988@gmail.com>
2017-09-11 09:57:11 +0200

    mapper interface

--- mapper.h
+++ mapper.h
@@ -32,8 +32,6 @@
 // Author: cxpan <chenxu.pan@fu-berlin.de>
 // ==========================================================================
 
-
-
 #include "base.h"
 #include "pmpfinder.h"
 
@@ -42,7 +40,7 @@
 
 template <typename TDna = typename MapperBase<>::DefaultAlphabet, 
     typename TSpec = typename MapperBase<>::DefaultShape>
-struct Mapper {
+class Mapper {
     typedef MapperBase<TDna, TSpec> Base;
     typedef typename Base::MRecord   Record;
     typedef typename Base::MParm     Parm;
@@ -52,6 +50,9 @@ struct Mapper {
     typedef typename Base::MSeq      Seq;
     typedef typename Base::MSeqs     Seqs;
     typedef typename Cord::CordSet      CordSet;
+    typedef typename Cord::CordType CordType;
+    typedef typename Res::HitSet    HitSet;
+    typedef typename Res::HitType   HitType; 
 
     Record  record;
     Parm    parm;
@@ -60,15 +61,21 @@ struct Mapper {
     CordSet cordSet;
 
     //StringSet<String<uint64_t> > cords;
-
+public:
     Mapper();
     Mapper(Options & options);
-    Seqs & reads(){return record.seq1;}
-    Seqs & genomes(){return record.seq2;}
-    Parm & mapParm(){return parm;}
-    Res & result(){return res;}
-    Index & index(){return qIndex;}
-    CordSet & cords(){return cordSet;}
+    Seqs & reads() {return record.seq1;}             
+    Seqs & genomes() {return record.seq2;}             
+    Parm & mapParm() {return parm;}
+    Res & result() {return res;}
+    Index & index() {return qIndex;}
+    HitSet & hits() {return res.hits;}             //returns hit set
+    HitType getHitX (HitType const &) const;     //returns coordinates x,y of the hits 
+    HitType getHitY (HitType const &) const;     //type uint64_t
+    CordSet & cords() {return cordSet;}            //returns cord set 
+    CordType getCordX(CordType const &) const;   // returns coordinates x,y of the vertex of sliding window
+    CordType getCordY(CordType const &) const;   // type uint64_t 
+
     void printHits();
     void printResult();    
     void printParm();
@@ -82,6 +89,88 @@ struct Mapper {
 };
 
 
+template <typename TDna, typename TSpec>
+Mapper<TDna, TSpec>::Mapper(Options & options):
+    record(options),
+    parm(options),
+    qIndex(genomes())
+{}
+
+template <typename TDna, typename TSpec>
+int Mapper<TDna, TSpec>::createIndex()
+{
+    std::cerr << "Creating index \n";
+    _createQGramIndex(qIndex);
+    return 0;
+}
+
+template <typename TDna, typename TSpec>
+typename Mapper<TDna,TSpec>::HitType 
+Mapper<TDna, TSpec>::getHitX(typename Mapper<TDna, TSpec>::HitType const & hit) const
+{
+    return _DefaultCord.getCordX(_DefaultCord.hit2Cord(hit));
+}
+
+template <typename TDna, typename TSpec>
+typename Mapper<TDna,TSpec>::HitType 
+Mapper<TDna, TSpec>::getHitY(typename Mapper<TDna, TSpec>::HitType const & hit) const
+{
+    return _DefaultCord.getCordY(_DefaultCord.hit2Cord(hit));
+}
+
+template <typename TDna, typename TSpec>
+typename Mapper<TDna,TSpec>::CordType 
+Mapper<TDna, TSpec>::getCordX(typename Mapper<TDna, TSpec>::CordType const & cord) const
+{
+    return _DefaultCord.getCordX(cord);
+}
+
+template <typename TDna, typename TSpec>
+typename Mapper<TDna,TSpec>::CordType 
+Mapper<TDna, TSpec>::getCordY(typename Mapper<TDna, TSpec>::CordType const & cord) const
+{
+    return _DefaultCord.getCordY(cord);
+}
+
+template <typename TDna, typename TSpec>
+void Mapper<TDna, TSpec>::printHits()
+{
+    std::cout << "Hits: " << lengthSum(res.hits) << " in sum " << std::endl;
+    for (auto && hitStr : res.hits)
+    {
+        for (auto && hit : hitStr)
+            std::cout << getHitX(hit) << " " << getHiY(hit) << ", ";
+        std::cout << std::endl;
+    }
+}
+
+template <typename TDna, typename TSpec>
+void Mapper<TDna, TSpec>::printResult()
+{}
+
+template <typename TDna, typename TSpec>
+void Mapper<TDna, TSpec>::printParm()
+{
+    parm.print();
+}
+
+template <typename TDna, typename TSpec>
+void map(Mapper<TDna, TSpec> mapper)
+{
+    std::cerr << "Encapsulated version " << std::endl;
+    double time = sysTime();
+    mapper.createIndex();
+    resize(mapper.hits(), length(mapper.reads()));
+    resize(mapper.cords(), length(mapper.reads()));
+    mnMap<TDna, TSpec>(mapper.index(), mapper.reads(), _DefaultMapParm, mapper.hits());
+    path(mapper.hits(), mapper.reads(), mapper.genomes(), mapper.cords());
+
+    //mapper.printParm();
+    //mapper.printHits();
+    //_DefaultCord.print(mapper.cords);
+    //mapper.printResult();
+    std::cerr << "Time of mapping in sum [s] " << sysTime() - time << std::endl;
+}
 
 
 #endif
