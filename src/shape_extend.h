7b9e11f1cdd756e7c49dfa7895ebe3833c6222a5
cx <cxpan1988@gmail.com>
2019-03-10 16:00:18 +0100

    2.1

--- src/shape_extend.h
+++ src/shape_extend.h
@@ -75,7 +75,6 @@ typedef Minimizer<0, 0> SimpleMShape;
 // Class Shape<Minimizer>
 // ----------------------------------------------------------------------------
 
-
 //template <typename TValue, typename TSpec>>
 //typedef seqan::Shape<TValue, TSpec> Shape;
 
@@ -105,7 +104,6 @@ public:
         strand(0),
         leftChar(0),
         x(0)
-
     {}
 };
 
@@ -158,7 +156,6 @@ length(Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > const &me)
 {
     return me.span;
 }
-
 
 // ----------------------------------------------------------------------------
 // Function weight()
@@ -254,51 +251,11 @@ template <typename TValue, unsigned TSPAN, unsigned TWEIGHT, typename TSpec, typ
         me.x = 0;
         //}
 }
-/*
-template <unsigned TSPAN, unsigned TWEIGHT, typename TSpec, typename TIter>
- uint64_t hashInit(Shape<Dna5, Minimizer<TSPAN, TWEIGHT, TSpec> > &me, TIter const &it)
-{
-
-        SEQAN_ASSERT_GT((unsigned)me.span, 0u);
-
-        me.leftChar = 0;
-        //me.hValue = ordValue(*it);
-        me.hValue = 0;
-        me.crhValue = 0;
-        //hash_key = ((uint64_t)1 << (me.span*2 -2 )) - 1;
-        //hash_key3 = ((uint64_t)1 << (me.span*2 )) - 1;
-        
-        uint64_t k =0, count = 0; //COMP for complemnet value;
-    
-        while (count < me.span)
-        {
-            if (ordValue(*(it + k + count)) == 4)
-            {
-                k += count + 1;
-                count = 0;
-            }
-            else
-                count++;
-        }
-        unsigned bit = 2;
-        for (unsigned i = 0; i < me.span - 1; ++i)
-        {
-            uint64_t val = ordValue (*(it + k + i));
-            me.hValue = (me.hValue << 2) + val;
-            me.crhValue += ((COMP4 - val) << bit);
-            bit += 2;
-            
-        }
-        me.x = 0;
-        //}
-        return k;
-}
-*/
 
 template <unsigned TSPAN, unsigned TWEIGHT, typename TSpec, typename TIter>
  uint64_t hashInit(Shape<Dna5, Minimizer<TSPAN, TWEIGHT, TSpec> > &me, TIter const &it)
 {
-
+    std::cout << "hashInitxxx\n";
     SEQAN_ASSERT_GT((unsigned)me.span, 0u);
 
     me.leftChar = 0;
@@ -351,172 +308,13 @@ template <unsigned TSPAN, unsigned TWEIGHT, typename TSpec, typename TIter>
 }
 
 /*
-    template <typename TValue, unsigned TSPAN, unsigned TWEIGHT, typename TSpec, typename TIter>
-     typename Value< Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > >::Type
-    hashNext(Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > &me, TIter const &it)
-    {
-    SEQAN_CHECKPOINT
-        // remove first, shift left, and add next character
-        //typedef typename Value< Shape<TValue, TSpec> >::Type    THValue;
-        typedef typename Size< Shape<TValue, TSpec> >::Type        TSize;
-        unsigned t;
-        SEQAN_ASSERT_GT((unsigned)me.span, 0u);
-
-        me.hValue=((me.hValue & hash_key)<<2)+ordValue((TValue)*(it + ((TSize)me.span - 1)));
-
-        t = (((me.hValue & hash_key1) & ((me.hValue & hash_key1) << 1)) != 0)?__builtin_ctzll((me.hValue & hash_key1 )& ((me.hValue & hash_key1) << 1)):__builtin_ctzll(me.hValue & hash_key1);
-        //t += (WINDOW_BIT_SIZE<Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > >::VALUE
-        //  - t) & ((WINDOW_BIT_SIZE<Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > >::VALUE - t)
-        //  >> (sizeof(unsigned) * CHAR_BIT - 1));
-        //t = ((TWEIGHT << 1) < t)?t:(TWEIGHT << 1);
-        //std::cout << t << " " << (32 - TWEIGHT <<1) << " " << hash_key1 << std::endl;
-        me.XValue = me.hValue << (64 - t) >> (32 - TWEIGHT <<1);//WINDOW_BIT_SIZE<Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > >::VALUE;
-        
-        me.YValue = (me.hValue >> (t-1) << (64-t))+(me.hValue <<t>>t)+(t<<(TSPAN - TWEIGHT << 1));
-        return me.XValue;
-    }
-
-*/
-
-
-/*
-template <typename TValue, unsigned TSPAN, unsigned TWEIGHT, typename TSpec, typename TIter>
- typename Value< Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > >::Type
-hashNext(Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > &me, TIter const &it)
-{
-        typedef typename Size< Shape<TValue, TSpec> >::Type        TSize;
-        SEQAN_ASSERT_GT((unsigned)me.span, 0u);
-        uint64_t v1;
-        unsigned t,t2 , span = TSPAN << 1, weight = TWEIGHT << 1;
-        uint64_t v2 = ordValue((TValue)*(it + ((TSize)me.span - 1)));
-        me.hValue=((me.hValue & MASK<TSPAN * 2 - 2>::VALUE)<<2)+ v2;
-        me.crhValue=((me.crhValue >> 2) & MASK<TSPAN * 2 - 2>::VALUE) + ((COMP4 - v2) << (span - 2));
-        me.XValue = MASK<TSPAN * 2>::VALUE; 
-        me.YValue = me.XValue;
-        //me.x += v2 - me.first;
-
-        //me.first = v2;
-        for (unsigned k = 64-span; k <= 64 - weight; k+=2)
-        {
-            v1 = me.hValue << k >> (64-weight);
-            v2 = me.crhValue << k >> (64 - weight);
-            if(me.XValue > v1)
-            {
-                me.XValue=v1;
-                t = k;
-            }
-            if (me.YValue > v2)
-            {
-                me.YValue = v2;
-                t2 = k;
-            }
-        } 
-        if (me.XValue < me.YValue)
-        {
-            me.XValue = me.YValue;
-            t = t2;
-            me.YValue = (me.crhValue >> (64-t) << (64-t-weight)) +
-                    (me.crhValue & (((uint64_t)1 << (64-t-weight)) - 1)) + 
-                    (t<<(span - weight - 1));
-        }
-        else
-        {
-            me.YValue = (me.hValue >> (64-t) << (64-t-weight)) +
-                    (me.hValue & (((uint64_t)1<<(64-t-weight)) - 1)) + 
-                    (t<<(span - weight - 1));
-        }
-        return me.XValue; 
-}
-*/
-
-
-//single strand hashNext
-/*
-template <typename TValue, unsigned TSPAN, unsigned TWEIGHT, typename TSpec, typename TIter>
- typename Value< Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > >::Type
-hashNext(Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > &me, TIter const &it)
-{
-        typedef typename Size< Shape<TValue, TSpec> >::Type        TSize;
-        SEQAN_ASSERT_GT((unsigned)me.span, 0u);
-        uint64_t v1;
-        unsigned t, span = TSPAN << 1, weight = TWEIGHT << 1;
- 
-        me.hValue=((me.hValue & MASK<TSPAN * 2 - 2>::VALUE)<<2)+ordValue((TValue)*(it + ((TSize)me.span - 1)));
-        me.XValue = MASK<TSPAN * 2>::VALUE; 
-
-        //std::cout << "hash_key3" << hash_key3 << std::endl;
-        for (unsigned k = 64-span; k <= 64 - weight; k+=2)
-        //for (unsigned k = 64-span; k <= 64 - span + 1; k+=2)
-        {
-            v1 = me.hValue << k >> (64-weight);
-            if(me.XValue > v1)
-            {
-                me.XValue=v1;
-                //t=k;
-                t = k;
-            }
-        } 
-        me.YValue = (me.hValue >> (64-t) << (64-t-weight))+(me.hValue & (((uint64_t)1<<(64-t-weight)) - 1)) + (t<<(span - weight - 1));
-
-        return me.XValue; 
-}
-*/
-
-/*
- * double strand hashNext
- */
-/*
-template <typename TValue, unsigned TSPAN, unsigned TWEIGHT, typename TSpec, typename TIter>
- typename Value< Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > >::Type
-hashNext(Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > &me, TIter const &it)
-{
-    //typedef typename Size< Shape<TValue, TSpec> >::Type  TSize;
-    SEQAN_ASSERT_GT((unsigned)me.span, 0u);
-    uint64_t v1;
-    unsigned t, span = TSPAN << 1, weight = TWEIGHT << 1;
-    uint64_t v2 = ordValue((TValue)*(it + me.span - 1));
-    me.hValue=((me.hValue & MASK<TSPAN * 2 - 2>::VALUE)<<2)+ v2;
-    me.crhValue=((me.crhValue >> 2) & MASK<TSPAN * 2 - 2>::VALUE) + 
-                ((COMP4 - v2) << (span - 2));
-    me.XValue = MASK<TSPAN * 2>::VALUE; 
-    me.x += (v2 - me.leftChar) << 1;
-    me.leftChar = ordValue(*(it));
-    //printf("[debug]::hash %d\n", me.x);
-    //v2 = (me.x > 0)?me.hValue:me.crhValue;
-    if (me.x > 0)
-    {
-        v2 =me.hValue; 
-        me.strand = 0;
-    }
-    else 
-    {
-        v2 = me.crhValue;
-        me.strand = 1;
-    }
-    
-    for (unsigned k = 64-span; k <= 64 - weight; k+=2)
-    {
-        v1 = v2 << k >> (64-weight);
-        if(me.XValue > v1)
-        {
-            me.XValue=v1;
-            t = k;
-        }
-    } 
-    me.YValue = (v2 >> (64-t) << (64-t-weight)) +
-                (v2 & ((1ULL<<(64-t-weight)) - 1)) + 
-                (t << (span - weight - 1));
-    return me.XValue; 
-}
-*/
-
-/*
  * this hashNext function is for index only collect mini hash value [minindex]
  */ 
 template <typename TValue, unsigned TSPAN, unsigned TWEIGHT, typename TSpec, typename TIter>
  typename Value< Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > >::Type
 hashNext(Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > &me, TIter const &it)
 {
+    std::cout << "hashNextxxx\n";
     //typedef typename Size< Shape<TValue, TSpec> >::Type  TSize;
     SEQAN_ASSERT_GT((unsigned)me.span, 0u);
     uint64_t v1;
@@ -624,6 +422,8 @@ template <typename TValue, unsigned TSPAN, unsigned TWEIGHT, typename TSpec, typ
  typename Value< Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > >::Type
 hashNextX(Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > &me, TIter const &it)
 {
+
+    std::cout << "hashNextxxx\n";
     //typedef typename Size< Shape<TValue, TSpec> >::Type  TSize;
     SEQAN_ASSERT_GT((unsigned)me.span, 0u);
     uint64_t v1;
@@ -663,43 +463,6 @@ template <typename TValue, unsigned TSPAN, unsigned TWEIGHT, typename TSpec>
     return (me.YValue >> ((TSPAN - TWEIGHT) << 1));
 }
 
-/*
- * bit twiddling hash , first 1
- */
-/*
-template <typename TValue, unsigned TSPAN, unsigned TWEIGHT, typename TSpec, typename TIter>
- typename Value< Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > >::Type
-hashNext(Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > &me, TIter const &it)
-{
-
-         // remove first, shift left, and add next character
-        //typedef typename Value< Shape<TValue, TSpec> >::Type    THValue;
-        typedef typename Size< Shape<TValue, TSpec> >::Type        TSize;
-        SEQAN_ASSERT_GT((unsigned)me.span, 0u);
-        uint64_t v1, t = 0;// tmp;
-        //unsigned pre = 23;
- 
-        me.hValue=((me.hValue & hash_key)<<2)+ordValue((TValue)*(it + ((TSize)me.span - 1)));
-        me.XValue = hash_key3;
-        //tmp = me.hValue ^ ((me.hValue << 32)+(me.hValue >> 32));
-        //tmp = me.hValue ;
-                
-        //if (me.x == 0 || me.XValue < (me.hValue & hash_key2))
-        //{
-        for (unsigned k = 64-(me.span << 1) ; k <= 64 - (me.weight << 1); k+=2)
-        {
-            v1 = me.hValue << k >> (64-(me.weight<<1));
-            if(me.XValue > v1)
-            {
-                me.XValue=v1;
-                t=k;
-            }
-        } 
-        me.YValue = (me.hValue >> (64-t) << (64-t-(me.weight<<1)))+(me.hValue & (((uint64_t)1<<(64-t-(me.weight<<1))) - 1))+(t<<((me.span - me.weight) << 1));
-
-        return me.XValue; 
-}
-*/
 template <typename TValue, unsigned TSPAN, unsigned TWEIGHT, typename TSpec>
  uint64_t h2y(Shape<TValue, Minimizer<TSPAN, TWEIGHT, TSpec> > &me,  uint64_t h)
 {
@@ -741,50 +504,7 @@ template <unsigned TSPAN, unsigned TWEIGHT>
     return ((y &(~mask) & mask1)<< weight) + (x << (64 - t - weight)) + (y & mask);
 }
 
-/*
-//============================
-//This part is to optimize 25-mer index
-//============================
-struct HIndexTag;
-template <unsigned TSPAN, unsigned>
-struct Minimizer<TSPAN, WEIGHT, HIndexTag>
-{};
-
-template <typename TValue, unsigned TSPAN>
-class Shape<TValue, Minimizer<TSPAN, HIndexTag> >
-{
-public:
-    typedef typename seqan::Value<Shape>::Type THashValue;
-
-    unsigned span;
-    unsigned weight;
-    THashValue hValue;     //hash value 
-    THashValue XValue;     //minimizer 
-    THashValue YValue;     //Y(h,x)
-    bool vflag;
-
-    Shape():
-        span(TSPAN),
-        weight(TWEIGHT),
-        hValue(0),
-        XValue(0),
-        YValue(0),
-        vflag(false)
-    {}
-};
-
-*/
-
 }
-
-
-// namespace seqan
-
-
-
-
-
-
 
 #endif
 
