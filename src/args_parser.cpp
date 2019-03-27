b5b3d8d90877bbae9b0feb5cd528b04df818647a
cx <cxpan1988@gmail.com>
2019-03-27 22:09:35 +0100

    add args.h .cpp

---
+++ src/args_parser.cpp
@@ -0,0 +1,95 @@
+#include "args_parser.h"
+
+seqan::ArgumentParser::ParseResult
+parseCommandLine(Options & options, int argc, char const ** argv)
+{
+    // Setup ArgumentParser.
+    seqan::ArgumentParser parser("Linear");
+    // Set short description, version, and date.
+    setShortDescription(parser, "Alignment of SMRT sequencing read");
+    setVersion(parser, "1.0");
+    setDate(parser, "May 2018");
+
+    // Define usage line and long description.
+    addUsageLine(parser,
+                    "[\\fIOPTIONS\\fP] \"\\fIread.fa\\fP\" \"\\fIgnome.fa\\fP\"");
+    addDescription(parser,
+                    "Program for mapping raw SMRT sequencing reads to reference genome.");
+
+    // Argument.
+    addArgument(parser, seqan::ArgParseArgument(
+        seqan::ArgParseArgument::INPUT_FILE, "read"));
+    setHelpText(parser, 0, "Reads file .fa, .fasta");
+
+    addArgument(parser, seqan::ArgParseArgument(
+        seqan::ArgParseArgument::INPUT_FILE, "genome", true));
+    setHelpText(parser, 1, "Reference file .fa, .fasta");
+
+    addSection(parser, "Mapping Options");
+    addOption(parser, seqan::ArgParseOption(
+        "o", "output", "choose output file.",
+            seqan::ArgParseArgument::STRING, "STR"));
+    addOption(parser, seqan::ArgParseOption(
+        "s", "sensitivity", "Sensitivity mode. -s 0 normal {DEFAULT} -s 1 fast  -s 2 sensitive",
+            seqan::ArgParseArgument::INTEGER, "INT"));
+    addOption(parser, seqan::ArgParseOption(
+        "t", "thread", "Default -t 4",
+            seqan::ArgParseArgument::INTEGER, "INT"));
+    
+// mapping parameters for tunning 
+    addOption(parser, seqan::ArgParseOption(
+        "l1", "listn1", "mapping::listn1",
+            seqan::ArgParseArgument::INTEGER, "INT"));     
+    addOption(parser, seqan::ArgParseOption(
+        "l2", "listn2", "mapping::listn2",
+            seqan::ArgParseArgument::INTEGER, "INT"));   
+    addOption(parser, seqan::ArgParseOption(
+        "a1", "alpha1", "mapping::alpha1",
+            seqan::ArgParseArgument::DOUBLE, "DOUBLE"));        
+    addOption(parser, seqan::ArgParseOption(
+        "a2", "alpha2", "mapping::alpha2",
+            seqan::ArgParseArgument::DOUBLE, "DOUBLE"));  
+    addOption(parser, seqan::ArgParseOption(
+        "t1", "cordThr", "mapping::cordThr",
+            seqan::ArgParseArgument::DOUBLE, "DOUBLE"));
+    addOption(parser, seqan::ArgParseOption(
+        "t2", "senThr", "mapping::senThr",
+            seqan::ArgParseArgument::DOUBLE, "DOUBLE"));        
+    addOption(parser, seqan::ArgParseOption(
+        "p1", "par1", "options::p1",
+            seqan::ArgParseArgument::INTEGER, "INT")); 
+        
+    // Add Examples Section.
+////////////////////////
+    addTextSection(parser, "Examples");
+    addListItem(parser,
+                "\\fBlinear \\fP \\fIreads.fa genomes.fa\\fP",
+                "raw map reads.fa to genomes.fa");
+    addTextSection(parser, "Examples");
+    addListItem(parser,
+                "\\fBlinear\\fP \\fP-a \\fIreads.fa genomes.fa\\fP",
+                "align reads.fa to genomes.fa");
+
+    // Parse command line.
+    seqan::ArgumentParser::ParseResult res = seqan::parse(parser, argc, argv);
+
+    if (res != seqan::ArgumentParser::PARSE_OK)
+        return res;
+
+    getOptionValue(options.oPath, parser, "output");
+    getOptionValue(options.sensitivity, parser, "sensitivity");
+    getOptionValue(options.thread, parser, "thread");
+    
+    getOptionValue(options.listN, parser, "listn1");
+    getOptionValue(options.listN2, parser, "listn2");
+    getOptionValue(options.alpha, parser, "alpha1");
+    getOptionValue(options.alpha2, parser, "alpha2");
+    getOptionValue(options.cordThr, parser, "cordThr");
+    getOptionValue(options.senThr, parser, "senThr");
+    getOptionValue(options.p1, parser, "p1");
+    seqan::getArgumentValue(options.rPath, parser, 0);
+    seqan::getArgumentValue(options.gPath, parser, 1);
+
+    return seqan::ArgumentParser::PARSE_OK;
+
+}
---
+++ src/args_parser.h
@@ -0,0 +1,7 @@
+#ifndef LINEAR_HEADER_ARGUMENT_PARSER_H
+#define LINEAR_HEADER_ARGUMENT_PARSER_H
+#include <seqan/arg_parse.h>
+
+seqan::ArgumentParser::ParseResult
+parseCommandLine(Options & options, int argc, char const ** argv);
+
