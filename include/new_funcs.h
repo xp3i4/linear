46d270fcb4f5384f9767a961bafc212169aed303
sqrt <sqrt1024@gmail.com>
2020-11-19 14:19:34 +0800

    add new_func.h/cpp

---
+++ include/new_funcs.h
@@ -0,0 +1,17 @@
+#ifndef LINEAR_HEADER_NEW_FUNCS_H
+#define LINEAR_HEADER_NEW_FUNCS_H
+
+#include <seqan/bam_io.h>
+#include "cords.h"
+
+/*==================================================================
+=            Headers to include new functions for test            =
+    Once the functions have passed the tests, move the new functions
+    to the modules they belong to.
+    Keep this file as simple as possible to save time of compilation
+==================================================================*/
+
+
+
+/*=====  End of Headers to include new functions for test======*/
+#endif
